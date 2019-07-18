e27f993d038159b0be1240595d2800cf75fb8b28
HB <hbianan@gmail.com>
2019-07-17 23:35:21 -0400

    updated game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,8 @@
+//hiding the alert box by default
+$('.alert-container').hide();
+
 //an array with default topics
 var topics = ['panda', 'dog', 'cat', 'fish', 'lion', 'tiger', 'sea lion'];
-console.log(topics);
 
 //global variable for the data query
 var searchQuery;
@@ -22,8 +24,6 @@ function ShowTopicButton(dataQuery){
 
     //adds new button
     $('.sidebar').append(topicBtn);
-
-    // CheckDuplicatedTopic();
 }
 
 //a method that creates button for each default value in the array
@@ -39,57 +39,81 @@ $('#search-btn').on('click', function(event){
     //a variable to get the search input's value
     searchQuery = $('#search-box').val();
 
-    console.log("these are topics " + topics);
-
+    //calling the function that checks for a duplicated topic before adding
     CheckDuplicatedTopic();
     
+    //below this line overwrites the 10 gifs 
     $('#giphy-container .row').html('');
 });
 
 //a function that checks for a duplicated topic
 function CheckDuplicatedTopic(){
+
+    //if the query in the search input already includes in the topics array
     if(topics.includes(searchQuery)){
-        console.log("duplicated found!")
+
+        //an alert will appear which..
+        $('.alert-container').slideDown();
+        $('.alert-danger').html(searchQuery + " is already existed");
+
+    //if the query is not yet existing
     }else{
-         //the search input value is being added to the array
+         //then the search input value will be added to the array
          topics.push(searchQuery);
 
          //adds button with the search input value
          ShowTopicButton(searchQuery);
-         console.log(topics)
+         
+         //will also show the 10 gifs after the submission
          ShowGiphyResult();
+
+         //the alert will then hide if it's a success
+         $('.alert-container').slideUp();
     }
 }
 
-//a  click event for the buttons in the sidebar
+
+//a click event for the buttons in the sidebar
 $('.topic-btn').on('click', function(event){
     event.preventDefault();
-    console.log($(this).val());
 
+    //a variable with the value of the input text box
     searchQuery = $(this).val();
-    console.log("this is the searchQuery " + searchQuery);
-    
+
+    //shows the 10 gifs when the user clicks the button
     ShowGiphyResult();
+
+    //below this line overwrites the 10 gifs 
     $('#giphy-container .row').html('');
 });
 
-
+//a function that shows the giphy
 function ShowGiphyResult(){
+
     $.ajax({
         url : "http://api.giphy.com/v1/gifs/search?q=" + searchQuery + "&api_key=NBFFDFiAlmknq6pwxTbJJdrC3YQs31yb&limit=10",
         method : "GET"
     }).then(function(result){
 
-        console.log(result);
-        console.log("length of the result data is: " + result.data.length)
-
+        //a variable that gets the data
         var searchResult = result.data;
-        
+        console.log(result)
+        //each array in the data object...
         $.each(searchResult, function(index, value){
-            var image = $('<img>');
-            var imagePause = searchResult[index].images.fixed_height_still.url;
-            var imagePlay = searchResult[index].images.fixed_height.url;
-            var imageID = searchResult[index].id;
+            
+            //a variable with an image tag
+            var image = $('<img>'),
+
+            //a variable with a gif that is "not animated"
+                imagePause = searchResult[index].images.fixed_height_still.url,
+            //a variable with a gif that is "animated"
+                imagePlay = searchResult[index].images.fixed_height.url,
+            //a variable with the gif's id
+                imageID = searchResult[index].id,
+            //a variable with the gif's rating
+                imageRating = searchResult[index].rating;
+            
+            //adding attributes to the image tag
             image.attr('src', imagePause);
             image.attr('id', imageID);
             image.attr('data-state', 'still');
@@ -97,23 +121,41 @@ function ShowGiphyResult(){
             image.attr('data-animate', imagePlay)
             image.attr('class', 'giphy')
 
-                    
+            //a variable with a div tag that has a class attribute with a value of "col"
             var column = $('<div class="col">');
+
+            //attaching the current image and rating in the column variable
             column.prepend(image);
+            column.prepend("<p class='rating'>Rating: " + imageRating + "</p>");
+
+            //adding that column inside the html
             $('#giphy-container .row').append(column);
-            // $('#giphy-container .row').replaceWith($('#giphy-container .row .col'));
         });
         
-        //make the giphy still 
+        //making the giphy not animated on the first load until..
         $('.giphy').on('click', function(){
-            var state = $(this).attr('data-state')
+
+            //a variable with the "data-state" attribute of the current element that will be clicked
+            var state = $(this).attr('data-state');
+
+            //if the current state is "still" before the user clicks it
             if (state === "still") {
+                
+                //the source will change to animate url
                 $(this).attr("src", $(this).attr("data-animate"));
+                
+                //the data-state will set to animate
                 $(this).attr("data-state", "animate");
-              } else {
+
+            //if the current state is "animated" before the user clicks it
+            } else {
+
+                //the source will change from animate to still
                 $(this).attr("src", $(this).attr("data-still"));
+                
+                //the data-state will set to still
                 $(this).attr("data-state", "still");
-              }
+            }
         })
 
         //this clears the search input box after the query is being submitted
